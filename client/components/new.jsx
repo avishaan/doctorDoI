ba10a650149d303b2b8e3d53698b2c2957baef3b
Avishaan <avishaan@codeHatcher.com>
2016-02-06 23:31:09 -0600

    multi line input field with submit button

--- client/components/new.jsx
+++ client/components/new.jsx
@@ -1,25 +1,33 @@
 const {
   TextField,
   Divider,
-  Paper
+  Paper,
+  RaisedButton
 } = mui;
 
 const styles = {
   textfield: {
     marginLeft: 20
+  },
+  button: {
+    margin: "0, auto",
+    display: "block"
   }
 };
 
 New = React.createClass({
   render() {
     return (
-      <TextField
-        hintText="Tell us what ails you"
-        floatingLabelText="Description of symptoms"
-        multiLine={true}
-        fullWidth={true}
-        rows={4}
-      />
+      <div>
+        <TextField
+          hintText="Tell us what ails you"
+          floatingLabelText="Description of symptoms"
+          multiLine={true}
+          fullWidth={true}
+          rows={4}
+        />
+        <RaisedButton label="Submit" primary={true} style={styles.button} />
+      </div>
     );
   }
 });
