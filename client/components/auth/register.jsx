56f206f540c60a4bb7a94e5ce06ec6f97336f79c
Avishaan <avishaan@codeHatcher.com>
2016-03-22 16:03:00 -0500

    layout registration page

--- client/components/auth/register.jsx
+++ client/components/auth/register.jsx
@@ -1,8 +1,47 @@
+const {
+  RaisedButton,
+  TextField,
+  Slider,
+  Styles
+} = mui;
+
+const ThemeManager = Styles.ThemeManager;
+
+const styles = {
+  textfield: {
+    marginLeft: 20
+  },
+  button: {
+    margin: "0, auto",
+    display: "block"
+  }
+};
 Register = React.createClass({
+  onRegisterTap(e){
+  },
   render() {
     return (
       <div>
-        <h1>Register Form</h1>
+        <TextField
+          id="email"
+          hintText="type in your email address"
+          floatingLabelText="email"
+          fullWidth={true}
+        />
+        <br/>
+        <TextField
+          id="password"
+          hintText="pick a password"
+          floatingLabelText="password"
+          fullWidth={true}
+          type="password"
+        />
+        <br/>
+        <RaisedButton label="Register"
+          primary={true}
+          style={styles.button}
+          onTouchTap={this.onRegisterTap}
+        />
       </div>
     );
   }
